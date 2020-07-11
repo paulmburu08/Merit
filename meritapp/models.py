83439567a60f1553274eb298f8b2071edb682eb8
paulmburu08 <paulmburu08@gmail.com>
2020-07-11 22:40:55 +0300

    import User

--- meritapp/models.py
+++ meritapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 from tinymce.models import HTMLField
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
