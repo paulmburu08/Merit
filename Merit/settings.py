f3b90bd0e178f012b810e6b8a4c33ee8da2c5c25
paulmburu08 <paulmburu08@gmail.com>
2020-07-11 17:10:23 +0300

    add rest_framework

--- Merit/settings.py
+++ Merit/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'meritapp',
-    'bootstrap4'
+    'bootstrap4',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
