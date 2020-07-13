e0ed7c25e99a5dd0ee673acf755034a49e48f317
paulmburu08 <paulmburu08@gmail.com>
2020-07-13 14:50:19 +0300

    add new_profile view function

--- meritapp/views.py
+++ meritapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy,reverse
+from .forms import ProfileForm,ProjectForm
+from .models import Project,Profile
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def new_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return HttpResponseRedirect(reverse('profile',args=[str(request.user.id)]))
+
+    else:
+        form = ProfileForm()
+
+    return render(request, 'new_profile.html',{'form':form})
