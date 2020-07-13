2e70db937737430875c1c8b09d5c49eb3848b4a2
paulmburu08 <paulmburu08@gmail.com>
2020-07-13 14:18:51 +0300

    add Project model

--- meritapp/models.py
+++ meritapp/models.py
@@ -13,3 +13,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name
+
+class Project(models.Model):
+    title = HTMLField()
+    landing_page_image = CloudinaryField('image')
+    description = HTMLField()
+    link = HTMLField()
+    technologies = HTMLField()
+    collaborators = HTMLField()
+    post_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    profile = models.ForeignKey(Profile,on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.title
